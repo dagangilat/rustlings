66bdf6e6f6b9b8de683fc9965fa95c4a28f2e11e
dagangilat <dagan.gilat@gmail.com>
2020-12-07 21:15:52 +0200

    v8

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -4,8 +4,10 @@
 // I AM NOT DONE
 
 enum Message {
-    // TODO: implement the message variant types based on their usage below
-    Echo
+    Echo(String),
+    Move {x: u8, y: u8},
+    ChangeColor(u8, u8, u8),
+    Quit,
 }
 
 struct Point {
@@ -38,6 +40,12 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
+        match message {
+            Message::Echo(s) => self.echo(s),
+            Message::Move{x,y} => self.move_position(Point{x: x, y: y}),
+            Message::ChangeColor(r, g, b)=> self.change_color((r, g, b)),
+            Message::Quit => self.quit(),
+        }
     }
 }
 
@@ -46,15 +54,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_match_message_call() {
+    pub fn test_match_message_call() {
         let mut state = State {
             quit: false,
             position: Point { x: 0, y: 0 },
             color: (0, 0, 0),
         };
-        state.process(Message::ChangeColor((255, 0, 255)));
+        state.process(Message::ChangeColor(255, 0, 255));
         state.process(Message::Echo(String::from("hello world")));
-        state.process(Message::Move(Point { x: 10, y: 15 }));
+        state.process(Message::Move{ x: 10, y: 15 });
         state.process(Message::Quit);
 
         assert_eq!(state.color, (255, 0, 255));
@@ -62,4 +70,10 @@ mod tests {
         assert_eq!(state.position.y, 15);
         assert_eq!(state.quit, true);
     }
+}
+
+use tests::test_match_message_call;
+
+fn main() {
+    test_match_message_call();
 }
